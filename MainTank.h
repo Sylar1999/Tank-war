15616f9645d89fb00e532cfa68a61e3320865a41
Sylar <775236047@qq.com>
2017-07-28 10:24:04 +0800

    Day 1

---
+++ MainTank.h
@@ -0,0 +1,31 @@
+#ifndef __MAIN_TANK__  
+#define __MAIN_TANK__  
+
+#include "Tank.h"  
+
+class MainTank : public Tank
+{
+public:
+	MainTank()
+	{
+		m_x = 400;
+		m_y = 300;
+		m_color = WHITE;
+		m_dir = Dir::UP;
+		m_step = 2;
+	}
+
+	~MainTank() {}
+
+	// 设置行驶方向  
+	void SetDir(Dir dir);
+
+	void Display();
+	void Move();
+
+protected:
+	// 绘制坦克主体  
+	void DrawTankBody(int style);
+};
+
+#endif  
