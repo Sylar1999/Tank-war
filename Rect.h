2439f98d79d58afd866cd7e391ef9b03e681eb37
Sylar <775236047@qq.com>
2017-07-29 19:22:19 +0800

    day2

---
+++ Rect.h
@@ -0,0 +1,48 @@
+#ifndef __RECTANGLE_H__
+#define __RECTANGLE_H__
+
+#include "Point.h"
+
+class Rect
+{
+public:
+	//构造函数
+	Rect(int x1 = 0, int y1 = 0, int x2 = 0, int y2 = 0) :
+		m_startPoint(x1, y1), m_endPoint(x2, y2) {}
+	Rect(const Point p1, const Point p2) :
+		m_startPoint(p1), m_endPoint(p2) {}
+
+	//析构函数
+	~Rect() {}
+
+	//重载操作符=
+	Rect& operator=(const Rect& rect)
+	{
+		m_startPoint = rect.GetStartPoint();
+		m_endPoint = rect.GetEndPoint();
+
+		return *this;
+	}
+
+	void Set(const Point pStart, const Point pEnd);
+	void Set(int x1, int y1, int x2, int y2);
+
+	void SetStartPoint(const Point p);
+	void SetEndPoint(const Point p);
+
+	Point GetStartPoint()const;
+	Point GetEndPoint()const;
+
+	int GetWidth();
+	int GetHeight();
+
+private:
+
+	void Check();
+
+	Point m_startPoint;
+	Point m_endPoint;
+
+};
+#endif // !__RECTANGLE_H__
+
